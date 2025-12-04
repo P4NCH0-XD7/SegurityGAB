719df9333b0ad1e38886d672b34d8891fb8d00c1
Ivan07Aragon <ivanaragon914@gmail.com>
2025-12-04 17:26:49 -0500

    feat: algo de front

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,7 +1,8 @@
 // services/api.ts
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 export async function loginUser(email: string, password: string) {
-  const res = await fetch('http://localhost:3000/login', {
+  const res = await fetch(`${BASE_API_URL}/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
