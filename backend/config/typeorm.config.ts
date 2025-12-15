955bd14f7bb7b0c1b8cd464395b1c80d88e25af4
Ivan07Aragon <ivanaragon914@gmail.com>
2025-12-15 12:52:45 -0500

    fix(config): Remove dotenv to rely on platform environment variables

--- backend/config/typeorm.config.ts
+++ backend/config/typeorm.config.ts
@@ -1,7 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
